93a4f899a70a4c9f4603eb3c9d74a142733549e1
Francesco Buldo <f.buldo@riello-ups.com>
2024-11-25 14:30:21 +0100

    Enable copilot chat and map <leader>ft to open termina at current buffer directory

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
+
+-- Open buffer symbols in the Trouble window
 local map = LazyVim.safe_keymap_set
 map(
   "n",
@@ -9,3 +10,9 @@ map(
   "<cmd>Trouble lsp_document_symbols win.position=right<CR>",
   { desc = "(Trouble) Open buffer symbols" }
 )
+-- Override <leader>ft: open terminal in the current file's directory
+map("n", "<leader>ft", function()
+  local buffer_path = vim.api.nvim_buf_get_name(0)
+  local buffer_dir = vim.fn.fnamemodify(buffer_path, ":h")
+  Snacks.terminal(nil, { cwd = buffer_dir })
+end, { desc = "Terminal (Buffer dir)" })
--- lua/plugins/general_purpose.lua
+++ lua/plugins/general_purpose.lua
@@ -9,4 +9,15 @@ return {
       colorscheme = "catppuccin",
     },
   },
+  {
+    "folke/edgy.nvim",
+    opts = function(_, opts)
+      opts.right = opts.right or {}
+      table.insert(opts.right, {
+        ft = "copilot-chat",
+        title = "Copilot Chat",
+        size = { width = 50 },
+      })
+    end,
+  },
 }
