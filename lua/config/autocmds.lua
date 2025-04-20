1e67912969cae2a27d1ef41c39b9efd6af5d7af9
Francesco Buldo <francesco.buldo@nidec-asi.com>
2025-04-21 00:10:50 +0200

    Set Kitty terminal window title reporting the CWD

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local function set_kitty_window_title()
+  print("Setting window title")
+  local cwd = vim.fn.getcwd()
+  vim.fn.system("kitty @ set-window-title NeoVim - " .. cwd)
+end
+-- run on VimEnter
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  callback = set_kitty_window_title,
+})
+-- run set_kitty_window_title when the cwd changes
+vim.api.nvim_create_autocmd("DirChanged", {
+  pattern = "global",
+  callback = set_kitty_window_title,
+})
