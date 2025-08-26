88350aa38b8cd2c280f8b57501070837cc994222
Francesco Buldo <francesco.buldo@nidec-asi.com>
2025-08-26 17:08:46 +0200

    Modify default tabstop and shiftwidt to 4

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.python3_host_prog = "/home/fbuldo/.pyenv/versions/py3nvim/bin/python"
+local opt = vim.opt
+opt.shiftwidth = 4 -- Size of an indent
+opt.tabstop = 4 -- Number of spaces tabs count for
