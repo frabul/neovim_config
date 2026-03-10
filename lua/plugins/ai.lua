5252ae6481825c1aae6ff284d9bd5f1a16785daa
Francesco Buldo <francesco.buldo@nidec-asi.com>
2026-03-10 22:35:09 +0100

    feat(ai): add openrouter_no_reasoning to codecompanion

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -7,8 +7,9 @@ return {
     strategies = {
       inline = {
         adapter = {
-          name = "openrouter",
-          model = "anthropic/claude-haiku-4.5",
+          name = "openrouter_no_reasoning",
+          -- model = "google/gemini-3-flash-preview",
+          model = "z-ai/glm-5",
         },
       },
       chat = {
@@ -33,6 +34,45 @@ return {
               api_key = "OPENROUTER_API_KEY",
               chat_url = "/v1/chat/completions",
               models_endpoint = "/v1/models",
+            },
+            handlers = {
+              parse_message_meta = function(self, data)
+                local extra = data.extra
+                if extra and extra.reasoning then
+                  data.output.reasoning = { content = extra.reasoning }
+                  if data.output.content == "" then
+                    data.output.content = nil
+                  end
+                end
+                return data
+              end,
+            },
+            schema = {
+              ---@type CodeCompanion.Schema
+              model = {
+                order = 1,
+                mapping = "parameters",
+                type = "enum",
+                desc = "ID of the model to use. See the model endpoint compatibility table for details on which models work with the Chat API.",
+                default = "minimax/minimax-m2",
+                choices = function(self)
+                  local get_models = require("openrouter_get_models")
+                  return get_models(self)
+                end,
+              },
+            },
+          })
+        end,
+        openrouter_no_reasoning = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            env = {
+              url = "https://openrouter.ai/api",
+              api_key = "OPENROUTER_API_KEY",
+              chat_url = "/v1/chat/completions",
+              models_endpoint = "/v1/models",
+            },
+            parameters = {
+              reasoning = { enabled = false },
             },
             handlers = {
               parse_message_meta = function(self, data)
